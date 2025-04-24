70af3c6bcc9b132b0df0c09af335911e963f7350
Wdrew232 <wdrew232@gmail.com>
2025-04-24 01:49:26 +0000

    fixed dynamic issues

--- src/js/app.js
+++ src/js/app.js
@@ -23,43 +23,52 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables); // log current state
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+  // Cover logic
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.includeCover === false) {
+    cover = "<div class='cover'></div>";
+  }
+
+  // Build dynamic social media links
+  let socialMediaLinks = "";
+  if (variables.twitter) {
+    socialMediaLinks += `<li><a href="https://twitter.com/${variables.twitter}" target="_blank"><i class="fab fa-twitter"></i></a></li>`;
+  }
+  if (variables.github) {
+    socialMediaLinks += `<li><a href="https://github.com/${variables.github}" target="_blank"><i class="fab fa-github"></i></a></li>`;
+  }
+  if (variables.linkedin) {
+    socialMediaLinks += `<li><a href="https://linkedin.com/in/${variables.linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>`;
+  }
+  if (variables.instagram) {
+    socialMediaLinks += `<li><a href="https://instagram.com/${variables.instagram}" target="_blank"><i class="fab fa-instagram"></i></a></li>`;
+  }
+
+  // Build dynamic widget
+  document.querySelector("#widget_content").innerHTML = `
+        <div class="widget">
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || "Name Unknown"} ${variables.lastName ||
+    ""}</h1>
+          <h2>${variables.role || "Role Undefined"}</h2>
+          <h3>${variables.city || "City Unknown"}, ${variables.country ||
+    "Country Unknown"}</h3>
+          <ul class="${variables.socialMediaPosition || "position-right"}">
+            ${socialMediaLinks}
           </ul>
         </div>
-    `;
+      `;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
-    socialMediaPosition: "position-left",
-    // social media usernames
+    socialMediaPosition: "position-right",
     twitter: null,
     github: null,
     linkedin: null,
@@ -67,25 +76,28 @@ window.onload = function() {
     name: null,
     lastName: null,
     role: null,
-    country: null,
-    city: null
+    city: null,
+    country: null
   };
-  render(window.variables); // render the card for the first time
+  render(window.variables); // Initial rendering
 
-  document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
-          ? null
-          : this.value == "true"
+  document.querySelectorAll(".picker").forEach(function(picker) {
+    picker.addEventListener("change", function(event) {
+      const attribute = event.target.id;
+      let value = event.target.value;
+
+      // Parse boolean or null values
+      value =
+        value === "true"
           ? true
-          : this.value == "false"
+          : value === "false"
           ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+          : value === "" || value === "null"
+          ? null
+          : value;
+
+      window.variables[attribute] = value; // Update the variable
+      render(window.variables); // Re-render widget
     });
   });
 };
